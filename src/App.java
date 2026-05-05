6683622053e7d709978e9de91b4c909d13c97ca0
elenak07 <ekoleva73@gmail.com>
2026-05-05 17:40:12 +0000

    finished taskManager and did majority of app.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,39 @@
 // this is the main class that will bring the classes together with user input
 //
-public class App {
-    public static void main(String[] args) throws Exception {
 
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        taskManager manager = new taskManager();
+
+        System.out.println("Welcome to your Task Planner!");
+
+        while (true){
+            System.out.println("Enter task name: (press enter when done adding tasks)");
+            String name = scanner.nextLine();
+
+            if (name.equals(" ")){
+                break;
+            }
+
+            System.out.println("Enter task duration in minutes:");
+            int duration = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter category ");
+            String category = scanner.nextLine();
+
+            if (category.equals(" ")){
+                category = "uncategorized";
+            }
+
+            manager.addTask(name, duration, category);
+            System.out.println("Task added!");
+        }
+
+        System.out.println("Total time: " + manager.getTotalTime() + "minutes");
+        scanner.close();
     }
 }
--- src/taskManager.java
+++ src/taskManager.java
@@ -4,19 +4,40 @@
 
 public class taskManager {
     private dailySchedule schedule;
-    private List<String> taskCategories;
 
     public taskManager() {
-        
+        this.schedule = new dailySchedule();
+        //this.taskCategories = schedule.getTaskCategories();
     }
 
+    public void addTask(String name, int duration, String category) {
+        int defaultImportance = 3; // default importance level
 
+        Task newTask = new Task(name, duration, defaultImportance, category, false);
+        // for simplicity, we will add the task to the first available slot in the schedule
+        for (int day = 0; day < 7; day++) {
+            for (int hour = 0; hour < 24; hour++) {
+                if (schedule.getTaskAt(day, hour) == null) {
+                    schedule.addTaskAt(day, hour, newTask);
+                    return;
+                }
+            }
+        }
+    }
 
+    public void removeTask(int day, int hour) {
+        schedule.removeTaskAt(day, hour);
+    }
 
+    public void assignTask(int day, int hour, Task task) {
+        schedule.addTaskAt(day, hour, task);
+    }
 
+    public Task getTask(int day, int hour) {
+        return schedule.getTaskAt(day, hour);
+    }
 
-
-
-
-
+    public int getTotalTime() {
+        return schedule.getTotalTime();
+    }
 }
